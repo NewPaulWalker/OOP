1bc39b28edd6e190150abd05d234a6df3b6dbd16
NewPaulWalker <linzhida19@mails.ucas.ac.cn>
2021-09-12 17:27:36 +0800

    🧪 test(java.Welcome): week2, homework1
    第二次课上关于Java的基础

---
+++ Java/Welcome.java
@@ -0,0 +1,22 @@
+public class Welcome {
+    public static void main(String[] args){
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+        System.out.println("Welcome to Java!");
+    }
+}
+/**
+    1.  每个Java程序至少有一个类，类名首字母大写
+        且应和文件名一致？
+    2.  主方法是程序运行的入口
+        主方法不是类的必要组成，但是如果要运行一个类
+        则必须包含一个主方法
+    3.  语句以";"结束
+    4.  保留字/关键字
+        如class  
+    5.  块
+        一对大括号{}
+        有类块、方法块
+    6.  注释
+*/
